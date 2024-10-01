e30a54b31a3393e4bc36912c861d2773187bb7c1
abhaygoswami302 <varindersharma786786@gmail.com>
2024-10-01 15:20:27 +0530

    mocking js

--- app/__tests__/index.test.tsx
+++ app/__tests__/index.test.tsx
@@ -35,16 +35,13 @@ describe('Grocery Store App', () => {
     const amountInput = screen.getByLabelText(/amount/i);
     const submitButton = screen.getByRole('button', { name: /submit/i });
 
-    // Adding an item first
     fireEvent.change(nameInput, { target: { value: 'Oranges' } });
     fireEvent.change(amountInput, { target: { value: '5' } });
     fireEvent.click(submitButton);
 
-    // Now editing that item
     const editButton = screen.getByRole('button', { name: /edit/i });
     fireEvent.click(editButton);
 
-    // Changing the amount to 10
     fireEvent.change(amountInput, { target: { value: '10' } });
     fireEvent.click(submitButton);
 
@@ -57,23 +54,20 @@ describe('Grocery Store App', () => {
     const amountInput = screen.getByLabelText(/amount/i);
     const submitButton = screen.getByRole('button', { name: /submit/i });
 
-    // Adding an item first
     fireEvent.change(nameInput, { target: { value: 'Oranges' } });
     fireEvent.change(amountInput, { target: { value: '5' } });
     fireEvent.click(submitButton);
 
-    // Deleting the added item
+    
     const deleteButton = screen.getByRole('button', { name: /delete/i });
     fireEvent.click(deleteButton);
 
-    // Ensure the item is removed from the DOM
     const deletedItemElement = screen.queryByText(/oranges/i);
     expect(deletedItemElement).not.toBeInTheDocument();
   });
 
   test('shows no items message when grocery list is empty', () => {
     localStorage.clear();
-    // First, ensure no items are present
     const noItemsMessage = screen.getByText(/no items in the grocery list/i);
     expect(noItemsMessage).toBeInTheDocument();
   });
